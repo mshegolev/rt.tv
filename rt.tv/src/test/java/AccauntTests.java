f993765b038fff702ad36f87ca31b1105f8db9d9
mshegolev <mshegolev@gmail.com>
2016-04-02 00:39:24 +0300

    gg

---
+++ rt.tv/src/test/java/AccauntTests.java
@@ -0,0 +1,22 @@
+import org.testng.annotations.Test;
+import rithmtime.tv.tests.SmokeTest;
+import rithmtime.tv.tests.AccountData;
+
+/**
+ * Created by mikhail on 4/2/16.
+ */
+public class AccauntTests extends SmokeTest {
+    @Test(testName = "testAuthorizationSuccess",
+            description = "Login with correct account")
+    public void testAuthorizationSuccess() throws Exception {
+        openMainPage();
+        AccountData account = new AccountData();
+        account.username = "test001";
+        account.password = "";
+        authorization(account);
+        clickAuthorization();
+        assertTrue(getUsername().contains(account.username));
+        assertTrue(buttonExitEnabled(),"Button EXIT doesn't exist");
+        clickExit();
+    }
+}
