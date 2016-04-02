ef5cd9bb6f391bc4aa4080b9bd2997d9318f4328
mshegolev <mshegolev@gmail.com>
2016-04-02 04:26:57 +0300

    gg

--- rt.tv/src/test/java/AccauntTests.java
+++ rt.tv/src/test/java/AccauntTests.java
@@ -1,22 +1,28 @@
 import org.testng.annotations.Test;
-import rithmtime.tv.tests.SmokeTest;
 import rithmtime.tv.tests.AccountData;
 
 /**
  * Created by mikhail on 4/2/16.
  */
-public class AccauntTests extends SmokeTest {
+public class AccauntTests {
     @Test(testName = "testAuthorizationSuccess",
             description = "Login with correct account")
     public void testAuthorizationSuccess() throws Exception {
-        openMainPage();
-        AccountData account = new AccountData();
-        account.username = "test001";
-        account.password = "";
-        authorization(account);
-        clickAuthorization();
-        assertTrue(getUsername().contains(account.username));
-        assertTrue(buttonExitEnabled(),"Button EXIT doesn't exist");
-        clickExit();
+        AccountData ac = new AccountData();
+        ac.getPassByName("test002");
     }
+//
+//    @Test(testName = "testAuthorizationSuccess",
+//            description = "Login with correct account")
+//    public void testAuthorizationSuccess() throws Exception {
+//        openMainPage();
+//        AccountData account = new AccountData();
+//        account.username = "test001";
+//        account.password = "";
+//        authorization(account);
+//        clickAuthorization();
+//        assertTrue(getUsername().contains(account.username));
+//        assertTrue(buttonExitEnabled(),"Button EXIT doesn't exist");
+//        clickExit();
+//    }
 }
