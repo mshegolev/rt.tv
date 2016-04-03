672ba8b846c4096a81adbf2b4b3fe8363f2bd9ba
mshegolev <mshegolev@gmail.com>
2016-04-03 03:12:44 +0300

    gg

--- rt.tv/src/test/java/AccauntTests.java
+++ rt.tv/src/test/java/AccauntTests.java
@@ -1,28 +1,60 @@
 import org.testng.annotations.Test;
 import rithmtime.tv.tests.AccountData;
+import rithmtime.tv.tests.NovigationPages;
+
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
 
 /**
  * Created by mikhail on 4/2/16.
  */
-public class AccauntTests {
+public class AccauntTests extends NovigationPages{
+    AccountData ac= new AccountData();
+    //NovigationPages pages = new NovigationPages();
+
+
     @Test(testName = "testAuthorizationSuccess",
             description = "Login with correct account")
     public void testAuthorizationSuccess() throws Exception {
-        AccountData ac = new AccountData();
-        ac.getPassByName("test002");
     }
-//
-//    @Test(testName = "testAuthorizationSuccess",
-//            description = "Login with correct account")
-//    public void testAuthorizationSuccess() throws Exception {
-//        openMainPage();
-//        AccountData account = new AccountData();
-//        account.username = "test001";
-//        account.password = "";
-//        authorization(account);
-//        clickAuthorization();
-//        assertTrue(getUsername().contains(account.username));
-//        assertTrue(buttonExitEnabled(),"Button EXIT doesn't exist");
-//        clickExit();
-//    }
+
+
+    @Test(testName = "testAuthorizationWronglogin",
+            description = "Try login with incorrect username")
+    public void testAuthorizationWronglogin() throws Exception {
+        String username = "test002";
+        openMainPage();
+        authorization(username,"null");
+        assertTrue(checkUsername(username),"Creds has wrong value");
+    }
+
+    @Test(testName = "testAuthorizationWrongPassword",
+            description = "Login with incorrect password")
+    public void testAuthorizationWrongPassword() throws Exception {
+        openMainPage();
+        setUsername("test001");
+        setPassword("wrongpass");
+        clickAuthorization();
+        assertTrue(authMessage().contains("Неверный"),"Don't has warning about wrong authentication");
+        assertFalse(buttonExit().isDisplayed(),"Button EXIT doesn't exist");
+    }
+    @Test(testName = "testAuthorizationEmptyPassword",
+            description = "Login with empty password")
+    public void testAuthorizationEmptyPassword() throws Exception {
+        openMainPage();
+        AccountData account = new AccountData();
+        clickAuthorization();
+        assertTrue(authMessage().contains("Неверный"),"Don't has warning about wrong authentication");
+        assertFalse(buttonExit().isDisplayed(),"Button EXIT doesn't exist");
+    }
+    @Test(testName = "testAuthorizationEmptylogin",
+            description = "Login with empty username")
+    public void testAuthorizationEmptylogin() throws Exception {
+        openMainPage();
+        AccountData account = new AccountData();
+        clickAuthorization();
+        assertTrue(authMessage().contains("Неверный"),"Don't has warning about wrong authentication");
+        assertFalse(buttonExit().isDisplayed(),"Button EXIT doesn't exist");
+    }
+
 }
