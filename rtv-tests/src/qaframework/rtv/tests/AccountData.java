feb81c790b582421c4afed264eaa95f88bcad79e
m.shegolev <mshegolev@gmail.com>
2016-04-21 00:10:55 +0300

    dfg

---
+++ rtv-tests/src/qaframework/rtv/tests/AccountData.java
@@ -0,0 +1,15 @@
+package qaframework.rtv.tests;
+
+public class AccountData {
+	public String username;
+	public String password;
+
+	public AccountData() {
+	}
+
+	public AccountData(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
+
+}
