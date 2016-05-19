db86e081ddb0e18a82d7ed84a07925c8030324f7
m.shegolev <mshegolev@gmail.com>
2016-05-19 04:00:45 +0300

    RTV-17 add test

--- rtv-tests/src/qaframework/rtv/tests/VideoTests.java
+++ rtv-tests/src/qaframework/rtv/tests/VideoTests.java
@@ -2,13 +2,16 @@ package qaframework.rtv.tests;
 
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
+import qaframework.rtv.fw.ApplicationManager;
 
 /**
  * Created by nikolay.g on 21.04.2016.
  */
 public class VideoTests extends TestBase {
-    private char w=0;
+        char w = 0;
+        ApplicationManager app = new ApplicationManager();
 
     @Test(testName = "RTV-11", description = "Check player and autostart")
     public void authorizationWithRememberCheckBox() throws Exception {
@@ -22,20 +25,21 @@ public class VideoTests extends TestBase {
         //app.getNavigationHelper().getPlayerContainer();
         app.getNavigationHelper().clickButtonExit();
         }
-    @Test(testName = "RTV-17", description = "Check player and autostart")
+
+    @Test(testName = "RTV-17",singleThreaded = true, description = "Check player and autostart")
     public void checkPlayerOnMainPage() throws Exception {
         WebDriver driver = null;
         app.getNavigationHelper().openMainPage();
         Assert.assertTrue(app.getVideoHelper().clickButtonPlay(), "Button start don't work, video isn't stated");
-        while ( !(app.getVideoHelper().getDuration().equals("0:10")) && w < 30){
+        if (w < 30) while (!(app.getVideoHelper().getDuration().equals("0:10")) && w < 30) {
             Thread.sleep(1000);
             w++;
         }
+         else System.err.println("The bicycle has already stopped!");
+
         //TODO: add check video stream or url or screenshot
         Assert.assertTrue(app.getVideoHelper().clickButtonPause(), "Button start don't work, video isn't stated");
         Assert.assertEquals(app.getVideoHelper().getDuration(), "0:10");
-
-        //*------- add some tests
         //TODO: needed mouse move to iframe for open buttons panel
     }
 
