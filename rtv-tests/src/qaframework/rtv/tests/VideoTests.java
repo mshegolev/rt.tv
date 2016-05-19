8e15894e0f3085c14997607bbdd62340274ffbea
m.shegolev <mshegolev@gmail.com>
2016-05-19 04:17:02 +0300

    RTV-17 quickwin - rollback

--- rtv-tests/src/qaframework/rtv/tests/VideoTests.java
+++ rtv-tests/src/qaframework/rtv/tests/VideoTests.java
@@ -3,7 +3,6 @@ package qaframework.rtv.tests;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import qaframework.rtv.fw.ApplicationManager;
 
 /**
  * Created by nikolay.g on 21.04.2016.
@@ -25,22 +24,21 @@ public class VideoTests extends TestBase {
 
     @Test(testName = "RTV-17", singleThreaded = true, description = "Check player and autostart")
     public void checkPlayerOnMainPage() throws Exception {
-        ApplicationManager app2 = new ApplicationManager();
-        app2.getNavigationHelper().openMainPage();
-        Assert.assertTrue(app2.getVideoHelper().clickButtonPlay(), "Button start don't work, video isn't stated");
-        if (w < 30) while (!(app2.getVideoHelper().getDuration().equals("0:10")) && w < 30) {
+        app.getNavigationHelper().openMainPage();
+        Assert.assertTrue(app.getVideoHelper().clickButtonPlay(), "Button start don't work, video isn't stated");
+        if (w < 30) while (!(app.getVideoHelper().getDuration().equals("0:10")) && w < 30) {
             Thread.sleep(1000);
             w++;
         }
         else {
-            app2.stop();
+            app.stop();
             System.err.println("The bicycle has already stopped!");
         }
 
         //TODO: add check video stream or url or screenshot
-        Assert.assertTrue(app2.getVideoHelper().clickButtonPause(), "Button start don't work, video isn't stated");
-        Assert.assertEquals(app2.getVideoHelper().getDuration(), "0:10");
-        app2.stop();
+        Assert.assertTrue(app.getVideoHelper().clickButtonPause(), "Button start don't work, video isn't stated");
+        Assert.assertEquals(app.getVideoHelper().getDuration(), "0:10");
+        app.stop();
         //TODO: needed mouse move to iframe for open buttons panel
     }
 
