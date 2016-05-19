e0675202bfb333e58a91a4157cf25b93946fde7e
m.shegolev <mshegolev@gmail.com>
2016-05-19 03:29:56 +0300

    RTV-17 add test

--- rtv-tests/src/qaframework/rtv/tests/VideoTests.java
+++ rtv-tests/src/qaframework/rtv/tests/VideoTests.java
@@ -14,6 +14,18 @@ public class VideoTests extends TestBase {
     public void authorizationWithRememberCheckBox() throws Exception {
         WebDriver driver = null;
         app.getNavigationHelper().openMainPage();
+        AccountData account = new AccountData();
+        account.username = "test002";
+        account.password = "002test";
+        app.getAccountHelper().fillLoginForm(app, account);
+        app.getNavigationHelper().clickButtonLogin();
+        //app.getNavigationHelper().getPlayerContainer();
+        app.getNavigationHelper().clickButtonExit();
+        }
+    @Test(testName = "RTV-17", description = "Check player and autostart")
+    public void checkPlayerOnMainPage() throws Exception {
+        WebDriver driver = null;
+        app.getNavigationHelper().openMainPage();
         Assert.assertTrue(app.getVideoHelper().clickButtonPlay(), "Button start don't work, video isn't stated");
         while ( !(app.getVideoHelper().getDuration().equals("0:10")) && w < 30){
             Thread.sleep(1000);
@@ -25,7 +37,7 @@ public class VideoTests extends TestBase {
 
         //*------- add some tests
         //TODO: needed mouse move to iframe for open buttons panel
-        }
+    }
 
     private void loginMainPageUserAdmin(){
         app.getNavigationHelper().openMainPage();
