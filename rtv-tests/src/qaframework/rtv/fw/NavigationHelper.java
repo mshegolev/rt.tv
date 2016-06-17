f510dbd17d0fc81710c456e5b7a6f40aeaeacd92
anna <ankatimko@gmail.com>
2016-06-17 21:42:05 +0300

    1

--- rtv-tests/src/qaframework/rtv/fw/NavigationHelper.java
+++ rtv-tests/src/qaframework/rtv/fw/NavigationHelper.java
@@ -2,6 +2,8 @@ package qaframework.rtv.fw;
 
 import org.openqa.selenium.By;
 
+import qaframework.rtv.tests.AccountData;
+
 public class NavigationHelper extends HelperBase {
 
 	public NavigationHelper(ApplicationManager manager) {
@@ -47,7 +49,43 @@ public class NavigationHelper extends HelperBase {
 
 	//public  void getPlayerContainer() { driver.findElement(By.id("videoPlayer")).findElement(By.tagName("iframe"));}
 	public  String getPlayerContainer() {return driver.findElement(By.id("videoPlayer")).getTagName().toString();}
-
+	public String getabonementTypeContent() {return driver.findElement(By.id("abonementType")).findElement(By.tagName("h4")).getText();}
+	public  String getPlayerVideoContainer() {return driver.findElement(By.id("playerVideo")).getTagName().toString();}
+	public boolean gettableSheduleLightFirstRow() {
+        return driver.findElements(By.xpath(".//*[@id='tableSheduleLight']/table/tbody/tr[1]/td[1]")).get(0).getText() != null;
+    }
+	public String getstatus1Content(){return driver.findElement(By.id("status1")).getText(); }
+	
+	
+	
+	
+	
+	
 	public  String getGiftBlockWithoutGiftContent() {return driver.findElement(By.id("paramList")).getText();}
 	public  void clickGiftBlockFirstTable() {click(By.id("blockParams1"));}
+	public String getFirstRowContentGiftTable() {return driver.findElement(By.id("jan2016RadasTVTable")).findElement(By.className("table_param_name")).getText();}
+	
+	
+
+	
+	
+	public void clickIdPersoanlSubscribe() {click(By.id("personalSubscribe"));}
+	public void clickOpenPhoneSubscribeButton() {click(By.id("OpenPhoneSubscribeButton"));}
+	public String checkInputPhoneExistence() {
+		return driver.findElement(By.id("phone")).getTagName();
+	}
+	
+	public void fillPhone(String phone) {
+		type(By.id("phone"), phone);
+	}
+	public void clickSendConfirmCode(){click(By.id("sendConfirmCode"));}
+	public String getTextInsendCodeFailBlock(){return driver.findElement(By.id("sendCodeFail")).getText();}
+	public String getTextInAgainCodeSendedBlock(){return driver.findElement(By.id("againCodeSended")).getText();}
+
+	public void clickPersonalCioroicControl(){click(By.id("personalCioroicControl"));}
+	public String getTextInPersonalTsioroits(){return driver.findElement(By.className("block_cont")).findElement(By.tagName("p")).getText();}
+	
+	public String getTextButtonCioroic(){return driver.findElement(By.id("buttonCioroic")).getText();}
+	public void clickButtonCioroic(){click(By.id("buttonCioroic"));}
+	
 }
