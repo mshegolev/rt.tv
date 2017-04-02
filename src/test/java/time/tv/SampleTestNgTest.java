f44ba592b36d0269c785cbf482cf92a040d373a2
Mikhail Shchegolev <mshegolev@gmail.com>
2017-04-03 00:35:52 +0300

    add login fields

--- src/test/java/time/tv/SampleTestNgTest.java
+++ src/test/java/time/tv/SampleTestNgTest.java
@@ -28,6 +28,11 @@ public class SampleTestNgTest extends TestNgTestBase {
         Assert.assertTrue(homepage.getTitle().toString().equals("Ритмовремя-ТВ"));
         Assert.assertTrue(loginPage.getTitle().toString().equals("Ритмовремя-ТВ"));
         Thread.sleep(5000);
+        loginPage.setUsername("mshegolev");
+        loginPage.setPassword("passw0rd");
+        Assert.assertFalse(loginPage.isRememberMe());
+        loginPage.enablerememberMe();
+        Assert.assertTrue(loginPage.isRememberMe());
         Assert.assertTrue(loginPage.buttonLogin.isDisplayed());
         //homepage
 
--- src/test/java/time/tv/pages/LoginPage.java
+++ src/test/java/time/tv/pages/LoginPage.java
@@ -6,10 +6,13 @@ import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 
+import java.io.IOException;
+
 /**
  * Sample page
  */
 public class LoginPage extends Page {
+
 
 
     @FindBy(how = How.TAG_NAME, using = "h1")
@@ -19,9 +22,59 @@ public class LoginPage extends Page {
     @FindBy(id = "id__2_5")
     public WebElement buttonLogin;
 
-    public LoginPage(WebDriver webDriver) {
-        super(webDriver);
+    @FindBy(id = "id__2_3")
+    private WebElement usernameField;
+
+    @FindBy(id = "id__2_4")
+    private WebElement passwordField;
+
+    @FindBy (id = "USER_REMEMBER_frm")
+    private WebElement rememberMe;
+
+    @FindBy (xpath = ".//*[@id='app']/div[2]/div/div/form/div[3]/a[1]")
+    private WebElement forgetPassword;
+
+    @FindBy (id = ".//*[@id='app']/div[2]/div/div/form/div[3]/a[2]")
+    private WebElement registration;
+
+
+
+    public LoginPage setUsername(String username){
+        usernameField.sendKeys(username);
+        return this;
     }
 
+    public LoginPage setPassword(String password){
+        passwordField.sendKeys(password);
+        return this;
+    }
+    public boolean clickLoginBtn(){
+        if (buttonLogin.isDisplayed()) {buttonLogin.click(); return true;}
+        else System.out.println("Button Войти doesn't exist"); return false;
+    }
+
+    public boolean enablerememberMe(){
+        if (!rememberMe.isSelected()) {
+            rememberMe.click();
+            return true;
+        }
+         return true;
+    }
+    public boolean disablerememberMy(){
+        if (rememberMe.isSelected()) {rememberMe.click(); return true;}
+        return true;
+    }
+
+    public boolean isRememberMe(){
+        return rememberMe.isSelected();
+    }
+
+
+
+
+
+    public LoginPage(WebDriver webDriver) throws IOException {
+        super(webDriver);
+    }
 
 }
